36cb69899e999b8d437366d1a173134360958e19
iamfrego <mate_freg@tiscali.it>
2021-11-15 09:35:36 +0100

    GG

---
+++ la-molisana/src/components/PastaElement.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="card">
+      <img :src="image" alt="" />
+      <h3>{{ titolo }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: String,
+    titolo: String,
+  },
+};
+</script>
+
+
+<style lang="scss">
+
+.card{
+    background-color: white;
+    text-align: center;
+    h3{
+        padding: 0.5rem;
+    }
+}
+
+</style>
